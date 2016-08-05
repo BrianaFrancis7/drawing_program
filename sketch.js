566ac224cf787fe9ed699f205fdfbde47b97700f
Neil Lakin <neil.lakin@gmail.com>
2016-08-05 14:51:38 -0400

    chicken pen

---
+++ sketch.js
@@ -0,0 +1,65 @@
+
+
+function circle(x, y, diameter) {
+  ellipse(x, y, diameter, diameter);
+}
+
+function chicken(x, y) {
+  
+  stroke(0);
+  
+  //Plate Outside Rim
+  fill(50, 0, 100, 120);
+  ellipse(x+10, y+25, 200, 100);
+  
+  //Plate Inside Rim
+  fill(250, 80, 80, 100);
+  ellipse(x+10, y+25, 150, 70);
+  
+  //Chicken
+  fill(200, 90, 0);
+  ellipse(x, y, 130, 90);
+  
+   //Chicken Bone
+  fill(255, 255, 255);
+  rect(x+50, y+12, 40, 10)
+  
+  //Chicken Bone Circle
+  fill(255, 255, 255);
+  ellipse(x+90, y+12, 15);
+  
+   //Chicken Bone Circle
+  fill(255, 255, 255);
+  ellipse(x+90, y+20, 15);
+  
+  //Chicken Leg
+  fill(200, 90, 0);
+  ellipse(x+50, y+15, 50, 40);
+}
+
+var myRed = 10;
+var myGreen = 0;
+var myBlue = 255;
+
+function setup() {
+  createCanvas(640, 480);
+}
+
+function draw() {
+  noStroke();
+  fill(myRed, myGreen, myBlue);
+  circle(mouseX, mouseY, 10);
+  //chicken(mouseX, mouseY);
+  myRed = myRed + 0.5;
+  if (myRed > 255) {
+    myRed = 0;
+  }
+  myBlue = myBlue - 1;
+  if (myBlue <= 0) {
+    myBlue=255;
+  }
+}
+
+function mousePressed() {
+  chicken(mouseX, mouseY);
+}
